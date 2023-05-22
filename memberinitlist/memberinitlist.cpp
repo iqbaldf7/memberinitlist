ae0f5448de1648844c18334ff819ca0eaa0c7644
iqbaldf7 <ACER@LAPTOP-BIGAQTOQ>
2023-05-22 20:37:31 +0700

    Add project files.

---
+++ memberinitlist/memberinitlist.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class mahasiswa {
+private:
+	const int id;
+	string nama;
+	float nilai;
+
+
+public:
+	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) {		//memberi initialization List
+		//Defenisi
+	}
+
+	~mahasiswa() {
+		cout << "Id    = " << id << endl;
+		cout << "Nama  = " << nama << endl;
+		cout << "Nilai = " << nilai << endl;
+	}
+};
+
+int main() {
+	mahasiswa mhs(12, "Lia", 90.5);
+
+	return 0;
+}
